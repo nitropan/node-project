d99c1931ed5ec3fd94626a2e518c6770a0236056
Kris Fernando <kris@sparxinc.com>
2022-09-03 19:06:23 -0400

    env port commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
     res.send("Welcome to the homepage");
 })
 
-app.listen(80, () => {
-    console.log('listening to port: 3000');
+app.listen(port, () => {
+    console.log('listening to port: '+port);
 });
