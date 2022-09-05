be385b5e9b4094652c3b5e7d76d0c18412043131
Kris Fernando <kris@sparxinc.com>
2022-09-04 21:45:55 -0400

    update landing webpage wording

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the homepage");
+    res.send("Welcome to the Express Application");
 })
 
 app.listen(port, () => {
