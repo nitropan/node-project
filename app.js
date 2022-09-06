56eed57ac780826b832c6a03c07119185f9d1895
Kris Fernando <kris@sparxinc.com>
2022-09-06 07:00:40 -0400

    update app.js

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ app.get('/', (req, res) => {
     res.send("Welcome to the Express Application");
 })
 
-app.get('/healthcheck', (req, res) => {
-    res.send("healthly");
-})
-
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
     console.log('Application running on port:' + port + '/');
